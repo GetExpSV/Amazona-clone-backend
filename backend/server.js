f2b217734dd7f948302c5d5c0ee418af3edee25b
GetExpSV <savinykhvladislav@gamil.com>
2020-08-13 17:57:25 +0300

    add detail products

--- backend/server.js
+++ backend/server.js
@@ -6,4 +6,13 @@ app.get('/api/products', (req,res)=>{
     res.send(data.products)
 });
 
+app.get('/api/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(p => p._id === productId);
+    if(product)
+        res.send(product);
+    else
+        res.status(404).send({msg: 'Product Not Found.'})
+});
+
 app.listen(3005, ()=>{console.log('Server started att http//localhost:3005')});
